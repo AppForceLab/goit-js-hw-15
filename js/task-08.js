f0e6acc0a748764e5c4516354eb344a02bb1d9cf
Volodymyr Popov <appforcelab@gmail.com>
2023-10-14 17:43:50 +0100

    Finished  task 6-7 DZ15

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault(); 
+
+  const emailInput = this.elements.email;
+  const passwordInput = this.elements.password;
+
+  if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+    alert('Будь ласка, заповніть всі поля форми.');
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log(`Об'єкт в консоль:`); 
+    console.log(formData); 
+    this.reset(); 
+  }
+});
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("#controls input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  const initialSize = 30;
+  boxesContainer.innerHTML = "";
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = initialSize + i * 10 + "px";
+    box.style.height = initialSize + i * 10 + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  createBoxes(input.value);
+});
+
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
